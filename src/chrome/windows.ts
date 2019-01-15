3045e0910f94770ad165df3a0de6a16f4006a60c
Sam Macbeth <sam@cliqz.com>
2019-01-15 12:04:11 +0100

    tabs api fixes.

--- src/chrome/windows.ts
+++ src/chrome/windows.ts
@@ -17,13 +17,6 @@ type Window = {
 }
 
 const windowStore = new Map<number, Window>();
-windowStore.set(3, {
-  alwaysOnTop: false,
-  id: 3,
-  focused: true,
-  incognito: false,
-  tabs: [],
-});
 
 const windows = {
   getAll(getInfo, cb) {
@@ -37,7 +30,10 @@ const windows = {
   onFocusChanged: new EventSource('windows.onCreated'),
 };
 
-windows.onCreated.addListener((window: Window) => windowStore.set(window.id, window));
+windows.onCreated.addListener((window: Window) => {
+  windowStore.set(window.id, window);
+  window.tabs = window.tabs || [];
+});
 windows.onRemoved.addListener((windowId: number) => windowStore.delete(windowId));
 
 export default windows;
