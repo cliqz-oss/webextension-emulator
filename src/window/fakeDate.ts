cd23ab4ed43fd1c09425b3027de4470bfc9d5942
Sam Macbeth <sam@cliqz.com>
2019-02-08 16:49:02 +0100

    Fix FakeDate constructor when used by moment

--- src/window/fakeDate.ts
+++ src/window/fakeDate.ts
@@ -5,6 +5,8 @@ export default (initialTime: number = Date.now(), timeMultiplier: number = 1.0) 
     constructor(year?, monthIndex?, day?, hour?, minutes?, seconds? , milliseconds?) {
       if (arguments.length === 0) {
         super(((Date.now() - actualTime) * timeMultiplier) + initialTime);
+      } else if(arguments.length === 1 ) {
+        super(year);
       } else {
         super(year, monthIndex, day, hour, minutes, seconds, milliseconds);
       }
